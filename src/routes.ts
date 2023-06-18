5059ec1ef7132e4c3a9890751f768b195358fee1
patryk <patryk.kuszneruk1798@gmail.com>
2023-06-18 17:38:52 +0200

    added react dependencies

--- src/routes.ts
+++
@@ -1,9 +0,0 @@
-import {Express, Request, Response} from "express";
-import { createUserHandler } from "./controller/user.controller";
-
-function routes(app: Express) {
-    app.get('/health-check', (req: Request, res: Response) =>  res.sendStatus(200));
-    app.post('api/user', createUserHandler);
-}
-
-export default routes;
--- src/schema/user.schema.ts
+++
@@ -1,9 +0,0 @@
-import {object, z} from "zod";
-
-export const createUserSchema = object({
-    name: z.string(),
-    role: z.number(),
-    password: z.string(),
-    created_date: z.string().datetime()
-});
-
