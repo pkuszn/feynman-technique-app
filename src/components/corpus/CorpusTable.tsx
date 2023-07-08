51798db751d735e66c67e59d0f73dd12003af6bf
patryk <patryk.kuszneruk1798@gmail.com>
2023-07-08 12:58:04 +0200

    api & search list

--- src/components/corpus/CorpusTable.tsx
+++ src/components/corpus/CorpusTable.tsx
@@ -2,31 +2,93 @@ import React, { useState } from "react";
 import "./style-corpus.css";
 import { AiOutlineSearch } from "react-icons/ai";
 
-
 const CorpusTable: React.FC = () => {
   const [words, setWords] = useState([
-    {id: 1, name: "dupa", partOfSpeech: 1, createdDate: '2023-06-01 00:00:00', context: "scraper", link: "localhost"},
-    {id: 1, name: "a", partOfSpeech: 61, createdDate: '2022-06-01 00:00:00', context: "scraper", link: "localhost"},
-    {id: 1, name: "b", partOfSpeech: 1, createdDate: '1999-06-01 00:00:00', context: "asd", link: "x"},
-    {id: 1, name: "c", partOfSpeech: 5, createdDate: '2023-06-01 00:00:00', context: "scraper", link: "localhost"},
-    {id: 1, name: "d", partOfSpeech: 1, createdDate: '2023-06-01 00:00:00', context: "scraper", link: "localxxhost"},
-    {id: 1, name: "e", partOfSpeech: 1, createdDate: '2023-06-01 00:00:00', context: "scraper", link: "localhost"},
-    {id: 1, name: "f", partOfSpeech: 11, createdDate: '2223-06-01 00:00:00', context: "ds", link: "localhost"},
-    {id: 1, name: "hg", partOfSpeech: 1, createdDate: '2023-06-01 00:00:00', context: "scraper", link: "localhost"}
-  ])
+    {
+      id: 1,
+      name: "dupa",
+      partOfSpeech: 1,
+      createdDate: "2023-06-01 00:00:00",
+      context: "scraper",
+      link: "localhost",
+    },
+    {
+      id: 1,
+      name: "a",
+      partOfSpeech: 61,
+      createdDate: "2022-06-01 00:00:00",
+      context: "scraper",
+      link: "localhost",
+    },
+    {
+      id: 1,
+      name: "b",
+      partOfSpeech: 1,
+      createdDate: "1999-06-01 00:00:00",
+      context: "asd",
+      link: "x",
+    },
+    {
+      id: 1,
+      name: "c",
+      partOfSpeech: 5,
+      createdDate: "2023-06-01 00:00:00",
+      context: "scraper",
+      link: "localhost",
+    },
+    {
+      id: 1,
+      name: "d",
+      partOfSpeech: 1,
+      createdDate: "2023-06-01 00:00:00",
+      context: "scraper",
+      link: "localxxhost",
+    },
+    {
+      id: 1,
+      name: "e",
+      partOfSpeech: 1,
+      createdDate: "2023-06-01 00:00:00",
+      context: "scraper",
+      link: "localhost",
+    },
+    {
+      id: 1,
+      name: "f",
+      partOfSpeech: 11,
+      createdDate: "2223-06-01 00:00:00",
+      context: "ds",
+      link: "localhost",
+    },
+    {
+      id: 1,
+      name: "hg",
+      partOfSpeech: 1,
+      createdDate: "2023-06-01 00:00:00",
+      context: "scraper",
+      link: "localhost",
+    },
+  ]);
 
+  const [searchValue, setSearchValue] = useState<string>("");
 
-  //TODO: Za dużo width dla tabeli i jej diva
+  const searchHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchValue(event.target.value);
+  };
+
+  const data = {
+    nodes: words.filter((word) => word.name.toLowerCase().includes(searchValue.toLowerCase())),
+  };
+
   return (
-    <div className="corpus_table__component"> 
+    <div className="corpus_table__component">
       <h3 id="corpus_table__component-text">Korpus językowy aplikacji</h3>
-      <div className="corpus_table__component_search">
-        <input id="corpus_table__component_seach_input" placeholder="Szukaj słów"></input>
-      </div>
-      <div className="corpus_table__component-filter">
-
-      </div>
-      <table className='corpus_table__component-data'>
+      <input
+        id="corpus_table__component_search-input"
+        placeholder="Szukaj słów"
+        onChange={searchHandler}
+      ></input>
+      <table className="corpus_table__component-data">
         <thead>
           <tr>
             <th>id</th>
@@ -38,14 +100,14 @@ const CorpusTable: React.FC = () => {
           </tr>
         </thead>
         <tbody>
-        {words.map(word => (
-            <tr key={word.id}>
-              <td>{word.id}</td>
-              <td>{word.name}</td>
-              <td>{word.partOfSpeech}</td>
-              <td>{word.createdDate}</td>
-              <td>{word.context}</td>
-              <td>{word.link}</td>
+          {data.nodes.map((node) => (
+            <tr key={node.id}>
+              <td>{node.id}</td>
+              <td>{node.name}</td>
+              <td>{node.partOfSpeech}</td>
+              <td>{node.createdDate}</td>
+              <td>{node.context}</td>
+              <td>{node.link}</td>
             </tr>
           ))}
         </tbody>
---
+++ src/constants/api.ts
@@ -0,0 +1,7 @@
+export class BackendApi {
+    public static LINGUISTIC_CORPUS_FILLMENT: string = "/LinguisticCorpusFillment";
+
+    public static PART_OF_SPEECH: string = "/PartOfSpeech";
+    public static PART_OF_SPEECH_ALL: string = "/PartOfSpeech/all";
+    public static PART_OF_SPEECH_GET: string = "PartOfSpeech/get";
+}
